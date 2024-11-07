bb26d9ec0679972cdcf105f2c062d8f995746e00
Geronimo001 <szgym29@gmail.com>
2024-11-07 09:11:20 +0100

    New start

---
+++ BAU_JAVAFX/src/main/java/hu/unideb/inf/sfm/bau_javafx/BauJavafxApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.sfm.bau_javafx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BauJavafxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BauJavafxApplication.class, args);
+    }
+
+}
---
+++ BAU_JAVAFX/src/test/java/hu/unideb/inf/sfm/bau_javafx/BauJavafxApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.sfm.bau_javafx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BauJavafxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
