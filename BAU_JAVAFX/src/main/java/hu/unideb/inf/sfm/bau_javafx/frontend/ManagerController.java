501e4c5c701d112d1cd71bff40276331df247db6
Geronimo001 <szgym29@gmail.com>
2024-11-09 10:06:37 +0100

    Manager View added

---
+++ BAU_JAVAFX/src/main/java/hu/unideb/inf/sfm/bau_javafx/frontend/ManagerController.java
@@ -0,0 +1,34 @@
+package hu.unideb.inf.sfm.bau_javafx.frontend;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ManagerController {
+    @FXML
+    private Label forename;
+
+    @FXML
+    private Label reg_num;
+
+    @FXML
+    private Label surname;
+
+    @FXML
+    private Label username;
+
+    @FXML
+    void CreateUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DeleteUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void EditUser(ActionEvent event) {
+
+    }
+}
