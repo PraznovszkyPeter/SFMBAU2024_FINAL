ea6f72a035d44c88f790970319319ae611bffa58
Geronimo001 <szgym29@gmail.com>
2024-11-09 10:17:19 +0100

    Doctor View Added

---
+++ BAU_JAVAFX/src/main/java/hu/unideb/inf/sfm/bau_javafx/frontend/DoctorController.java
@@ -0,0 +1,34 @@
+package hu.unideb.inf.sfm.bau_javafx.frontend;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class DoctorController {
+    @FXML
+    private Label allatFaj;
+
+    @FXML
+    private Label allatSzam;
+
+    @FXML
+    private Label emailCim;
+
+    @FXML
+    private Label gazdaNev;
+
+    @FXML
+    private Label kertVizsgalat;
+
+    @FXML
+    private Label megjegyzes;
+
+    @FXML
+    private Label telefonszam;
+
+    public void showDoctor(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void closeVisit(javafx.event.ActionEvent actionEvent) {
+    }
+
+}
