7ad67a08e71097568341cf7fe40ddd2682b4f34d
Pigeon 🕊 <iterg2013@gmail.com>
2024-11-16 19:45:44 +0100

    home js style

--- BAU_REACT/src/Home.js
+++ BAU_REACT/src/Home.js
@@ -2,17 +2,40 @@ import { useState } from "react";
 import Reservation from "./Reservation";
 
 const Home = () => {
+    const [hoverText, setHoverText] = useState("");
 
-    return ( 
-        <div class="home">
-            <img src={require("./baulog.jpg")} alt="logo" className="logo" id="pic"/>
+    const handleHover = (text) => setHoverText(text);
+    const clearHover = () => setHoverText("");
 
+    return (
+        <div className="home" style={{ textAlign: "center", padding: "1rem" }}>
+            {/* Logo */}
+            <img
+                src={require("./log_00.png")}
+                alt="logo"
+                className="logo"
+                id="pic"
+                style={{
+                    width: "325px",
+                    marginBottom: "1.5rem",
+                   // boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+                }}
+            />
+
+            {/* Bevezető szöveg */}
             <div>
-                <h2>Eddig beérkezett foglalásaink</h2>
+                
+            <h1
+                 style={{color: "#12681a", marginBottom: "1rem", textShadow: "0 0 3px #fff, 0 0 5px #fff",  }}>
+                        Üdvözlünk a BAU állatorvosi klinika weboldalán!
+                        </h1>
+                <p style={{ fontSize: "1.3rem", color: "#fff" }}>
+                    Nálunk egyszerűen és gyorsan foglalhat időpontot szakértő orvosainkhoz!
+                </p>
             </div>
-
+           
         </div>
-     );
-}
- 
+    );
+};
+
 export default Home;
