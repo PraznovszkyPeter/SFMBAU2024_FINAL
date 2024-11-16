9d14639609bb0550aff13020ad2cc8b8e231f07b
Pigeon 🕊 <iterg2013@gmail.com>
2024-11-16 19:44:34 +0100

    docs js style

--- BAU_REACT/src/Docs.js
+++ BAU_REACT/src/Docs.js
@@ -1,29 +1,40 @@
+import './index';
+import vet1 from './images/vet1.jpg';
+import vet2 from './images/vet2.jpg';
+import vet3 from './images/vet3.jpg';
+
+
 import { useState } from "react";
 
     document.title = "Docs";
 
 const Docs = () => {
     const [docs, setDocs] = useState([
-        {nev: "Dr. Kis Béla", szakterulet: "sebész", telefonszam: "0620836293", email: "kisbela@gmail.com", id: 1},
-        {nev: "Dr. Nagy József", szakterulet: "altató",telefonszam: "0630834723", email: "nagyjozsef@gmail.com", id: 2},
-        {nev: "Dr. Gipsz Jakab", szakterulet: "sebész",telefonszam: "0620345389", email: "gipszj@gmail.com", id: 3}
+        {nev: "Dr. Kis Béla", szakterulet: "sebész", telefonszam: "0620836293", email: "kisbela@gmail.com", id: 1, kep: vet1 },
+        {nev: "Dr. Kovács Laura Szilvia", szakterulet: "altató",telefonszam: "0630834723", email: "kocsacslau@gmail.com", id: 2,  kep: vet2},
+        {nev: "Dr. Nagy Eszter", szakterulet: "sebész",telefonszam: "0620345389", email: "nagye@gmail.com", id: 3,  kep: vet3}
     ]);
 
     return ( 
         <div className="orvosok">
-            {docs.map((doc) => (
-                <div key={doc.id}>
-                    <h2>{doc.nev}</h2>
-                    <p>Szakterület: {doc.szakterulet}</p>
-                    <p>Mobil: {doc.telefonszam}</p>
-                    <p>Email: {doc.email}</p>
-                </div>
-            ))}
-        </div>
+        {docs.map((doc) => (
+            <div key={doc.id}>
+                <h2>{doc.nev}</h2>
+                <p>Szakterület: {doc.szakterulet}</p>
+                <p>Mobil: {doc.telefonszam}</p>
+                <p>Email: {doc.email}</p>
+                <img 
+                    src={doc.kep} 
+                    alt={`Dr. ${doc.nev}`} 
+                    className="orvos-kep" 
+                />
+            </div>
+        ))}
+    </div>
+    
         );
     
-
-    
+   
 }
  
 export default Docs;
